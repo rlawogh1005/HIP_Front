ea52471a6f3c07117c65269edcf45a18a9919568
jhk <qwertyjhk11@naver.com>
2024-12-22 16:46:50 +0900

    수정: 프로젝트 요청 및 응답 인터페이스에서 'class' 필드를 'className'으로 변경

--- campus/src/app/models/project/projects/projects-request.interface.ts
+++ campus/src/app/models/project/projects/projects-request.interface.ts
@@ -2,14 +2,14 @@ import { ProjectStatus } from '../../enums/role.enums';
 
 export interface CreateProjectRequest {
     topic: string;
-    class: string;
+    className: string;
     team_name: string;
     generation: string;
 }
 
 export interface UpdateProjectRequest {
     topic?: string;
-    class?: string;
+    className?: string;
     status?: ProjectStatus;
     team_name?: string;
     generation?: string;
