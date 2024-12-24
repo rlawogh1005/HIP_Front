63e10c2c3f3ec7afdc5d7425c5db264409ef629e
jhk <qwertyjhk11@naver.com>
2024-12-24 13:05:19 +0900

    추가: project-my-left-bottom 모듈 및 컴포넌트 생성

---
+++ campus/src/app/component/projectmy/project-my-left-bottom/project-my-left-bottom.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectMyLeftBottomComponent } from './project-my-left-bottom.component';
+
+@NgModule({
+    declarations: [
+      ProjectMyLeftBottomComponent
+    ],
+    imports: [
+        CommonModule,
+    ],
+    exports: [ProjectMyLeftBottomComponent]
+  })
+  export class ProjectMyLeftBottomModule {}
+  
