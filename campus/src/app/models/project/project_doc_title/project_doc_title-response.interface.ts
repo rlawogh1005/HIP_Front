d9e0d2bb7d740b13e3a8efff8b9e866d5631be0d
jhk <qwertyjhk11@naver.com>
2024-12-24 17:08:26 +0900

    추가: project_doc_title-response.interface.ts 파일 생성 및 ProjectDocTitleResponseData 인터페이스 정의

---
+++ campus/src/app/models/project/project_doc_title/project_doc_title-response.interface.ts
@@ -0,0 +1,9 @@
+import { ProjectDocResponseData } from "../project_doc/project_doc-response.interface";
+
+export interface ProjectDocTitleResponseData {
+    project_doc_id: number;
+    title: string;
+    pa_title_id: number;
+    project_docs: ProjectDocResponseData[];
+    sub_titles: ProjectDocTitleResponseData[];
+}
