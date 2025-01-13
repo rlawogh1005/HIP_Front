f7db5ff9c19212419d0c8cff2a00ac0ff2e40afb
jhk <qwertyjhk11@naver.com>
2025-01-13 16:20:08 +0900

    추가: openUploadModal 작성

--- campus/src/app/component/projectmy/project-my-left-top/project-my-left-top.component.ts
+++ campus/src/app/component/projectmy/project-my-left-top/project-my-left-top.component.ts
@@ -1,32 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { LoadingController, NavController } from '@ionic/angular';
+import { LoadingController, ModalController } from '@ionic/angular';
 import { firstValueFrom } from 'rxjs';
 import { ApiResponse } from 'src/app/models/common/api-response.interface';
 import { ProjectResponseData } from 'src/app/models/project/projects/projects-response.interface';
 import { ProjectService } from 'src/app/services/project/project.service';
+import { UploadProjectFileComponent } from '../../upload-project-file/upload-project-file.component';
 
 @Component({
   selector: 'app-project-my-left-top',
   templateUrl: './project-my-left-top.component.html',
   styleUrls: ['./project-my-left-top.component.scss'],
 })
-export class ProjectMyLeftTopComponent  implements OnInit {
+export class ProjectMyLeftTopComponent implements OnInit {
   projects?: ProjectResponseData;
   project_id: number = 0;
+
   constructor(
     private projectService: ProjectService,
-    private navCtrl: NavController,
+    private modalController: ModalController, // ModalController 주입
     private loadingController: LoadingController,
     private route: ActivatedRoute
-  ) { }
+  ) {}
 
   refreshPage() {
     window.location.reload();
   }
-  
+
   ngOnInit(): void {
-    // Route에서 project_id를 가져오고 이후 loadProject 호출
     this.route.params.subscribe((params) => {
       this.project_id = Number(params['project_id']);
       console.log('URL project_id:', this.project_id);
@@ -40,23 +41,32 @@ export class ProjectMyLeftTopComponent  implements OnInit {
 
   async loadProject(): Promise<void> {
     try {
-      // 로딩 컨트롤러 표시
       const loading = await this.loadingController.create({
         message: '프로젝트를 불러오는 중...',
       });
       await loading.present();
 
-      // API 호출 및 데이터 설정
       const response: ApiResponse<ProjectResponseData> = await firstValueFrom(
         this.projectService.getOneProject(this.project_id)
       );
       this.projects = response.data;
       console.log('프로젝트 데이터:', this.projects);
 
-      // 로딩 컨트롤러 해제
       await loading.dismiss();
     } catch (error) {
       console.error('프로젝트 로드 중 오류 발생:', error);
     }
+  }
+
+  async openUploadModal(key_doc_category: string): Promise<void> {
+    console.log('project-my-left-top-component key_doc category: ', key_doc_category);
+    const modal = await this.modalController.create({
+      component: UploadProjectFileComponent, // 모달로 띄울 컴포넌트
+      componentProps: {
+        projectId: this.project_id, // 프로젝트 ID를 props로 전달
+        projectKeyDocCategory: key_doc_category
+      }
+    });
+    return await modal.present();
   }
 }
